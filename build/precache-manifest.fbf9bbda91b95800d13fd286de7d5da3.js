1c369f5db58ef1b5de5260c9feb0275189873c04
khai@com <khai@gmail.com>
2020-02-17 23:37:15 +0700

    add index

---
+++ build/precache-manifest.fbf9bbda91b95800d13fd286de7d5da3.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "54688cba8cbe1daabb3c",
+    "url": "http://localhost:8000/static/js/0.f69abf1a.chunk.js"
+  },
+  {
+    "revision": "f94bd67d2368b7a06759",
+    "url": "http://localhost:8000/static/css/main.65c9d7e8.chunk.css"
+  },
+  {
+    "revision": "f94bd67d2368b7a06759",
+    "url": "http://localhost:8000/static/js/main.06403a76.chunk.js"
+  },
+  {
+    "revision": "3304940e04cb4cd54d33",
+    "url": "http://localhost:8000/static/js/runtime~main.15f8473f.js"
+  },
+  {
+    "revision": "51efb7dcbca8a5836b68",
+    "url": "http://localhost:8000/static/js/3.30d9226d.chunk.js"
+  },
+  {
+    "revision": "fcd30badbc4ff2809684",
+    "url": "http://localhost:8000/static/js/4.8a3a2dd8.chunk.js"
+  },
+  {
+    "revision": "06f077ce0d5bdee50bf2",
+    "url": "http://localhost:8000/static/js/5.6ebaae17.chunk.js"
+  },
+  {
+    "revision": "bd82343630b14c09623d",
+    "url": "http://localhost:8000/static/js/6.cf9a5b2c.chunk.js"
+  },
+  {
+    "revision": "e447f0d769648e7ecd7d",
+    "url": "http://localhost:8000/static/js/7.d72b3a07.chunk.js"
+  },
+  {
+    "revision": "64dee6756901877b5663",
+    "url": "http://localhost:8000/static/js/8.b4592139.chunk.js"
+  },
+  {
+    "revision": "168b59f69ba6870dd0fe",
+    "url": "http://localhost:8000/static/js/9.1730c612.chunk.js"
+  },
+  {
+    "revision": "0b9200fa89558764865a",
+    "url": "http://localhost:8000/static/js/10.71b33dc8.chunk.js"
+  },
+  {
+    "revision": "036eb203471a7619931e",
+    "url": "http://localhost:8000/static/js/11.2efbf512.chunk.js"
+  },
+  {
+    "revision": "6a3151dbe200a349f056",
+    "url": "http://localhost:8000/static/js/12.b37b81b0.chunk.js"
+  },
+  {
+    "revision": "b01e20342f662167aa87",
+    "url": "http://localhost:8000/static/js/13.54952bc5.chunk.js"
+  },
+  {
+    "revision": "810603b30777ccca0534",
+    "url": "http://localhost:8000/static/js/14.aeb9799d.chunk.js"
+  },
+  {
+    "revision": "3edd78d912b6bf63a870",
+    "url": "http://localhost:8000/static/js/15.0f0faaf9.chunk.js"
+  },
+  {
+    "revision": "3dbf5ed8b80122aaa608",
+    "url": "http://localhost:8000/static/js/16.ea6fc620.chunk.js"
+  },
+  {
+    "revision": "4e4114ac69403855a156",
+    "url": "http://localhost:8000/static/js/17.b3d0dedc.chunk.js"
+  },
+  {
+    "revision": "a96e254a9f79de775636",
+    "url": "http://localhost:8000/static/js/18.76dea645.chunk.js"
+  },
+  {
+    "revision": "bf4629170109672b6cf0",
+    "url": "http://localhost:8000/static/js/19.442b2c34.chunk.js"
+  },
+  {
+    "revision": "116f5b762bdc1a5b60fc",
+    "url": "http://localhost:8000/static/js/20.f0036676.chunk.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "http://localhost:8000/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "http://localhost:8000/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "http://localhost:8000/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "http://localhost:8000/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "http://localhost:8000/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "http://localhost:8000/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "http://localhost:8000/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "http://localhost:8000/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "http://localhost:8000/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "http://localhost:8000/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "http://localhost:8000/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "http://localhost:8000/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "http://localhost:8000/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "http://localhost:8000/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "http://localhost:8000/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "http://localhost:8000/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "http://localhost:8000/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "http://localhost:8000/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "http://localhost:8000/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "http://localhost:8000/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "http://localhost:8000/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "http://localhost:8000/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "http://localhost:8000/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "http://localhost:8000/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "http://localhost:8000/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "http://localhost:8000/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "http://localhost:8000/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "http://localhost:8000/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "http://localhost:8000/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "http://localhost:8000/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "http://localhost:8000/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "0bf6e73cac313ffa3e5755532d7ead5b",
+    "url": "http://localhost:8000/index.html"
+  }
+];
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bodyParser=require('body-parser')
+const cors = require('cors')
+const path=require('path')
+
+const app = express();
+const port = 8000;
+const SECRET = 'shhh';
+
+//app.use(express.json()) // for parsing application/json
+app.use(cookieParser()) //cookie-parser dùng để đọc cookies của request:
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.post('/auth/login', (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        console.log('body>>', req.body);
+        if(email !== 'e' || password !== 'p'){
+            res.status(400);
+            throw new Error('invalid infor')
+        }
+        const payload = {
+            email: email
+        }
+        const token = jwt.sign(payload, SECRET);
+        
+        res.cookie('access_token', token, {
+            maxAge: 3000,
+            httpOnly: true,
+            // secure: true;
+        })
+        let a=new Date().getTime()
+        a.toString().slice(0,-3)
+        res.status(200).send({email,password,time:a})
+
+    
+})
+
+
+app.use('/api/users', (req, res) => {
+    const token = req.cookies.access_token;
+
+    console.log('cookie>>>', token);
+    try{
+        let a=Object.create(null)
+        decoded = jwt.verify(token, SECRET);
+        res.status(200).json(decoded);
+        console.log(decoded)
+    }catch(err){
+        res.status(400)
+        throw err;
+    }
+    
+})
+app.use("/build", express.static('./build/'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use((err, req, res, next)=> {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(port, () => {
+    console.info(`listening on port ${port}`);
+})
